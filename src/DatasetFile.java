be884bbbbf05eac80984e9cb3371c5d7716baeed
AndyChen <andy565866@gmail.com>
2018-01-15 20:51:13 +0800

    Implement Patient class.

---
+++ src/DatasetFile.java
@@ -0,0 +1,15 @@
+public class DatasetFile implements FactorsProvider {
+
+    public DatasetFile(String fileName) {
+        this.fileName = fileName;
+    }
+
+    @Override
+    public double getFactor() {
+        return _factors[_currentFactorIndex ++];
+    }
+
+    String fileName;
+    int _currentFactorIndex = 0;
+    double[] _factors = {150, 123, -1, 200, -1};
+}
